f025e86831fe4ea6235f2cd01c6e1bb0efd51261
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 02:14:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,9 +19,15 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       @status = "complete"
+    else
+      reject_transfer
     end
     
   end
   
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
   
 end
