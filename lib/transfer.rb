e5a9f36be573e6ac2860144b1733f192e4f97986
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 02:12:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
     if self.valid?
       sender.balance -= @amount
       receiver.balance += @amount
-      status = "complete"
+      self.status = "complete"
     end
     
   end
