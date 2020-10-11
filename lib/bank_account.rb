facc66153eb2206994a668260e013d3aaf95e316
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 01:49:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,15 +3,16 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
+  
+
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
   end
 
-  def deposit
-    
-    
+  def deposit(money)
+    @balance << money
   end
 
 
